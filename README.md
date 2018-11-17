89bfd24de76241904aab7aa874f880b9a229896f
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-11-17 01:44:22 -0800

    Release v0.2.0

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ by adding `echo_boy` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:echo_boy, "~> 0.1.0"}
+    {:echo_boy, "~> 0.2.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,11 +4,25 @@ defmodule EchoBoy.MixProject do
   def project do
     [
       app: :echo_boy,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
+      package: package(),
       start_permanent: Mix.env() == :prod,
+      description: description(),
       deps: deps()
     ]
+  end
+
+  defp description do
+    "HTTP and WebSocket echo server as an OTP Application"
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["Alex Kwiatkowski"],
+      links: %{"GitHub" => "https://github.com/rupurt/echo_boy"}
+    }
   end
 
   def application do
@@ -23,6 +37,7 @@ defmodule EchoBoy.MixProject do
       {:cowboy, "~> 2.5.0"},
       {:plug, "~> 1.7.0"},
       {:plug_cowboy, "~> 2.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:websockex, "~> 0.4", only: :test},
       {:httpoison, "~> 1.4.0", only: [:dev, :test]}
     ]
