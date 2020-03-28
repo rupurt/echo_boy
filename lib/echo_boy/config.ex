d089fbd2b099ef8083924aa01f0cb0fd01d624d4
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-03-28 12:14:17 -0700

    Default to port 4200

--- lib/echo_boy/config.ex
+++ lib/echo_boy/config.ex
@@ -1,5 +1,8 @@
 defmodule EchoBoy.Config do
+  @default_port 4200
+
+  @spec port :: non_neg_integer
   def port do
-    Application.get_env(:echo_boy, :port)
+    Application.get_env(:echo_boy, :port, @default_port)
   end
 end
